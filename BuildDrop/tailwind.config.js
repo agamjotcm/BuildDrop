1a662042e67aa4382c732929f436068c881e02ea
Agamjot Singh <23610@rc.school.nz>
2024-03-21 11:04:35 +1300

    Added Header

---
+++ BuildDrop/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+darkMode: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
