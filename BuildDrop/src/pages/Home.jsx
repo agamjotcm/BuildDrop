60c66a5cffc66dfeb5cdfddf7bff8afd166b33be
Agamjot13Singh <agamjotsingh943@gmail.com>
2024-04-05 16:03:45 +1300

    Local Computer Merge

--- BuildDrop/src/pages/Home.jsx
+++ BuildDrop/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import './css/Home.css'
 
 export class Home extends Component {
   render() {
     return (
       <>
       <Header />
-      <div>
+      <div className='home'>
         Homesj
       </div>
       </>
